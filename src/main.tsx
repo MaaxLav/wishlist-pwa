e82b11ef53436a1f8d1a10387e68e2e25c94fe1a
MaxVein <makslav67@gmail.com>
2025-07-17 22:06:48 +0300

    update firebase app init

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,6 @@ import "./index.css";
 import App from "./App.tsx";
 import { register } from "./serviceWorkerRegistration.ts";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBtU7vXkUmc5jNxFQHCsq8-0gIZJ61mfpI",
-  authDomain: "wishlist-pwa.firebaseapp.com",
-  projectId: "wishlist-pwa",
-  storageBucket: "wishlist-pwa.firebasestorage.app",
-  messagingSenderId: "542728530822",
-  appId: "1:542728530822:web:7126969671642cef005a07",
-};
-
-// Initialize Firebase
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
@@ -26,4 +11,3 @@ createRoot(document.getElementById("root")!).render(
 );
 
 register();
-initializeApp(firebaseConfig);
